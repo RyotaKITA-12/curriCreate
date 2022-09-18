a00bdc7e31b802ab631003444afad0806e6d1d45
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-09-18 14:51:14 +0900

    [server]add post

--- app/app.py
+++ app/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return "index page"
+    name = request.args.get("name")
+    animals = ["dog", "cat"]
+    return render_template("index.html", name=name, animals=animals)
+
+
+@app.route("/index", methods=["post"])
+def post():
+    name = request.form["name"]
+    animals = ["dog", "cat"]
+    return render_template("index.html", name=name, animals=animals)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

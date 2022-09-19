d2607890aa5b2c5fa4a2eac2a6cb3e7e1668a148
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-09-20 05:53:30 +0900

    [server]add total-credit

--- app/app.py
+++ app/app.py
@@ -30,6 +30,17 @@ def get_path_db():
     return path, config
 
 
+def calculate_total_credits():
+    total = credits_required
+    for s_id in session['select_id']:
+        df_credit = pd.read_sql(
+            sql='SELECT credit_required, credit_elective FROM curriculums WHERE curriculum_id = 3',
+            con=connection
+        )
+        total += [sum(elem) for elem in df_credit.values.tolist()][0]
+    return total
+
+
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(minutes=10)
 
@@ -44,37 +55,62 @@ HEADER_CURRICULUM = ["entrance_year", "course_id", "course_category",
                      "credit_elective", "course_summary", "course_keyword"]
 
 ENTRANCE_YEAR = "2020"
-app.secret_key = 'selects'
+app.secret_key = 'select_id'
+
+
+df_credit_required = pd.read_sql(
+    sql=f"SELECT credit_required FROM curriculums WHERE credit_elective=0 AND entrance_year={ENTRANCE_YEAR}",
+    con=connection
+)
+credits_required = sum([elem[0] for elem in df_credit_required.values.tolist()])
 
 
 @app.route("/")
 @app.route("/index")
 def index():
+    session['select_id'] = []
+    select_list = session['select_id']
+    credits_total = calculate_total_credits()
     df_category = pd.read_sql(
         sql='SELECT course_keyword FROM keywords;',
         con=connection
     )
     record_category = set([elem[0] for elem in df_category.values.tolist()])
     return render_template("index.html",
-                           categorys=record_category)
+                           categorys=record_category,
+                           total=credits_total,
+                           select_list=select_list)
 
 
 @app.route("/", methods=["POST"])
 @app.route("/index", methods=["POST"])
 def post_index():
     select_id = request.form.getlist("elem")
+    if 'select_id' not in session:
+        session['select_id'] = []
+    select_list = session['select_id']
+    for i in select_id:
+        select_list.append(i)
+    session['select_id'] = select_list
+    credits_total = calculate_total_credits()
     df_category = pd.read_sql(
         sql='SELECT course_keyword FROM keywords;',
         con=connection
     )
     record_category = set([elem[0] for elem in df_category.values.tolist()])
     return render_template("index.html",
-                           categorys=record_category)
+                           categorys=record_category,
+                           total=credits_total,
+                           select_list=select_list)
 
 
 @app.route("/selects", methods=["POST"])
 def selects():
     select_categorys = request.form.getlist("elem")
+    if 'select_id' not in session:
+        session['select_id'] = []
+    select_list = session['select_id']
+    credits_total = calculate_total_credits()
     sql = "SELECT * FROM curriculums"
     sql += " INNER JOIN keywords"
     sql += " ON curriculums.curriculum_id = keywords.course_id"
@@ -89,7 +125,9 @@ def selects():
     record = df_curriculum.values.tolist()
     return render_template("select.html",
                            selects=select_categorys,
-                           record=record)
+                           record=record,
+                           total=credits_total,
+                           select_list=select_list)
 
 
 if __name__ == "__main__":

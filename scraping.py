779ebdf8458dae15e88448382a3b3c0cf0b017dd
ShotaTamaru <71062618+tama2101-s@users.noreply.github.com>
2022-09-19 20:54:48 +0900

    スクレイピングプログラムの改良とアウトプット

--- scraping.py
+++ scraping.py
@@ -3,9 +3,13 @@ from time import sleep
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import requests
 import csv
 import keyword_extract
+import re
+import pandas as pd
+
 
 #use as headless mode
 options = Options()
@@ -13,8 +17,8 @@ options = Options()
 driver = webdriver.Chrome("../chromedriver",options=options)
 class_list = []
 
-USERID = 'id'
-PASSWORD = 'pass'
+USERID = 's2022022'
+PASSWORD = '_7j_ZwM8'
 
 # driver = webdriver.Chrome('/Users/hyugataki/Univ/B3/dbd/scraping/chromedriver')
 error_flg = False
@@ -37,64 +41,77 @@ sleep(1)
 syllabusSearch = driver.find_element(by=By.XPATH, value="/html/body/div/div[1]/div[6]/ul/li[4]/ul/li[1]/a").get_attribute("href")
 driver.get(syllabusSearch)
 sleep(1)
+lec_all = []
+    #講義を抽出
+LecList = []
+OverviewList = []
 
 dropdown = driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/table/tbody/tr/td[1]/form/div[3]/div[2]/table/tbody/tr[5]/td[2]/select')
 select = Select(dropdown)
 select.select_by_index(1)
 driver.find_element(by=By.XPATH, value="//*[@id='srch_search']").click()
 sleep(1)
-
-    #講義を抽出
-LecList = []
-OverviewList = []
+english_lec = ["英語基礎A","英語基礎B","英語基礎C","英語基礎D","フィールド・スポーツ"]
 j = 1
-while j < 41:
-    j += 1
-    for i in range(3, 23): #20件取得する23
-        lec_list = "//*[@id='form']/div[4]/table/tbody/tr[" + str(i) + "]/td[3]/a"
-        driver.find_element(by=By.XPATH, value=lec_list).click()
-        sleep(2)
-        lec = driver.find_element(by=By.XPATH, value="//*[@id='form']/div[2]/table/tbody/tr[3]/td[3]")
-        # print(lec.text)
-        ll = []
-        ll.append(lec.text)
-        print(ll)
-        LecList.append(ll)
-        # print(LecList)
-        sleep(1)
-        overview = driver.find_element(by=By.XPATH, value="//*[@id='form']/div[3]/table/tbody/tr[1]/td[3]/table/tbody/tr/td")
-        lll = []
-        lll.append(overview.text)
-        OverviewList.append(lll)
-        # print(overview.text)
-        # OverviewList.append(OV)
-        # OverviewList.append(overview.text)
+TF = True
+try:
+    while TF == True:
+        j += 1
+        for i in range(3, 23): #20件取得する23
+            kari = []
+            try:
+                lec_list = "//*[@id='form']/div[4]/table/tbody/tr[" + str(i) + "]/td[3]/a"
+                lec_data = driver.find_element(by=By.XPATH, value=lec_list)
+            except NoSuchElementException:
+                TF = False
+                break
+            
+            # sleep(2)
+            # lec = driver.find_element(by=By.XPATH, value="//*[@id='form']/div[2]/table/tbody/tr[3]/td[3]")
+            lec = lec_data.text
+            lec_sprit = re.sub('\[.*?\]','',lec)
+            print(j)
+            
+            if lec_sprit in english_lec:
+                class_list.append(lec_sprit)
+                lec = lec
+            elif lec_sprit in class_list:
+                if i == 22: #次のページに
+                    driver.execute_script("doPaging('form','navigateKougiList','pageCount','"+str(j)+"','maxCount','20');return false;")
+                    # driver.execute_script("doPaging('form','navigateKougiList','pageCount','80','maxCount','20');return false;")⇦これ以上ない場合のテスト
+                else:
+                    continue
+                continue
+            else:
+                class_list.append(lec_sprit)
+                lec = lec_sprit
+            # print(lec.text)
+            lec_data.click()
+            ll = []
+            kari.append(lec)
 
-        driver.find_element(by=By.XPATH, value="//*[@id='back']").click()
-        sleep(1)
-        if i == 22: #次のページに
-            p = j + 1
-            nxtP = "//*[@id='form']/div[4]/table/tbody/tr[1]/td/div[2]/span[2]/a[" + str(p) + "]"
-            driver.find_element(by=By.XPATH, value=nxtP).click()
-        else:
-            continue
-else:
-    print("DONE")
+            sleep(1)
+            overview = driver.find_element(by=By.XPATH, value="//*[@id='form']/div[3]/table/tbody/tr[1]/td[3]/table/tbody/tr/td")
+            kari.append(overview.text)
 
-f = open("test.csv", 'w', newline='')
-writer = csv.writer(f)
-writer.writerows(LecList)
-f.close()
-print("出力完了")
+            keyword = '・'.join(keyword_extract.run(lec,overview.text))
+            kari.append(keyword)
+            
+
+            driver.find_element(by=By.XPATH, value="//*[@id='back']").click()
+            sleep(1)
+            lec_all.append(kari)
+            if i == 22: #次のページに
+                driver.execute_script("doPaging('form','navigateKougiList','pageCount','"+str(j)+"','maxCount','20');return false;")
+            else:
+                continue
+    else:
+        print("DONE")
 
 
-f = open("muscat.csv", 'w', encoding='utf-8')
-writer = csv.writer(f,lineterminator='\n')
-writer.writerows(OverviewList)
-f.close()
-print("出力完了")
-
-f2 = open("muscat_title.csv", 'w', encoding='utf-8')
-writer = csv.writer(f2,lineterminator='\n')
-writer.writerows(LecList)
-f2.close()
+    df = pd.DataFrame(lec_all,columns=["lec","overvier","keyword"])
+    df.to_csv("output.csv",index=False)
+except:
+    df = pd.DataFrame(lec_all,columns=["lec","overvier","keyword"])
+    df.to_csv("output.csv",index=False)
+    

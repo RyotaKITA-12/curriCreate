4e4affb79a51c5affe9aa16f2110c736faee0db0
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-09-19 13:50:21 +0900

    [server]link template-server

--- app/app.py
+++ app/app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    categorys = ["データサイエンス", "機械学習", "ゲーム", "IoT",
+                 "Web", "深層学習", "強化学習", "数学", "スクレイピング",
+                 "倫理", "データベース", "音声認識", "画像認識", "自然言語処理"]
+    return render_template("index.html", categorys=categorys)
 
 
 @app.route("/list")
